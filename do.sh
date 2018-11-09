e4d4477d4c4612f4bab2e654d7588671f95e2f41
斉藤 <saito@saitounoMacBook-Pro.local>
2018-11-09 14:07:35 +0900

    add .sh

---
+++ do.sh
@@ -0,0 +1,2 @@
+/Users/saito/anaconda/bin/python /Users/saito/Documents/work/emoji/face_crop.py $2 $3 $4 $5
+/Users/saito/anaconda/bin/python /Users/saito/Documents/work/emoji/mk_mosaic.py $1 $2 $4 $5
